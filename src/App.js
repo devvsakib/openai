8bf5647008f2cc08b9e3a698e889b853a0d7dc19
DevvSakib <devvsakib@gmail.com>
2022-12-16 01:32:09 +0600

    generate page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './index.css'
 import Help from './pages/Help'
 import Account from './pages/Account'
 import Header from './components/Headers/Header'
+import Generate from './pages/Generate'
 const App = () => {
     return (
         <>
@@ -12,6 +13,7 @@ const App = () => {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='help' element={<Help />} />
+                <Route path='generate' element={<Generate />} />
                 <Route path='account' element={<Account />} />
             </Routes>
         </>
